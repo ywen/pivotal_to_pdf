a9aca885139653c916f6f48e453cde8fd58b72d9
Yi Wen <hayafirst@gmail.com>
2011-06-23 23:09:35 -0500

    test the travis hooks

--- lib/pivotal_to_pdf/pivotal.rb
+++ lib/pivotal_to_pdf/pivotal.rb
@@ -1,6 +1,7 @@
 class Pivotal < ActiveResource::Base
   def self.inherited(sub)
-    info = YAML.load_file File.expand_path("~/.pivotal.yml")
+    base_dir = ENV["PIVOTAL_TO_PDF_CONFIG_DIR"] || "~"
+    info = YAML.load_file File.expand_path("#{base_dir}/.pivotal.yml")
     sub.site = %Q|https://www.pivotaltracker.com/services/v3/projects/#{info['project_id']}|
     sub.headers['X-TrackerToken'] = info["token"]
   end
