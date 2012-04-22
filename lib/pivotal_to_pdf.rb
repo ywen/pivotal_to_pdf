b6fc68cbcede8b7446ab63f11123e2f1ccc709fa
Yi Wen <hayafirst@gmail.com>
2012-04-21 22:24:02 -0500

    Use FormatterFactory.formatter

--- lib/pivotal_to_pdf.rb
+++ lib/pivotal_to_pdf.rb
@@ -4,6 +4,7 @@ require 'rainbow'
 require 'thor'
 require 'active_resource'
 require 'pivotal_to_pdf/configure'
+require 'pivotal_to_pdf/formatter_factory'
 require 'pivotal_to_pdf/simple_text_formatter'
 require 'pivotal_to_pdf/pivotal'
 require 'pivotal_to_pdf/iteration'
@@ -16,22 +17,22 @@ module PivotalToPdf
     class << self
       def story(story_id)
         story = Story.find(story_id)
-        DefaultFormatter.new([ story ]).write_to(story_id)
+        FormatterFactory.formatter.new([ story ]).write_to(story_id)
       end
 
       def current_iteration
         iteration = Iteration.find(:all, :params => {:group => "current"}).first
-        DefaultFormatter.new(iteration.stories).write_to("current")
+        FormatterFactory.formatter.new(iteration.stories).write_to("current")
       end
 
       def iteration(iteration_number)
         iteration = Iteration.find(:all, :params => {:offset => iteration_number.to_i - 1, :limit => 1}).first
-        DefaultFormatter.new(iteration.stories).write_to(iteration_number)
+        FormatterFactory.formatter.new(iteration.stories).write_to(iteration_number)
       end
 
       def label(label_text)
         stories = Story.find(:all, :params => {:filter => "label:\"" + label_text + "\""})
-        DefaultFormatter.new(stories).write_to(label_text)
+        FormatterFactory.formatter.new(stories).write_to(label_text)
       end
     end
   end
--- spec/pivotal_to_pdf_spec.rb
+++ spec/pivotal_to_pdf_spec.rb
@@ -3,12 +3,25 @@ require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
 module PivotalToPdf
   describe Main do
     let(:writer) {double :writer, :write_to => true}
+    before(:each) do
+      FormatterFactory.stub(:formatter).and_return DefaultFormatter
+    end
+    
+    shared_examples "formatter fetching" do
+      it "asks the FormatterFactory for the formatter" do
+        FormatterFactory.should_receive(:formatter).and_return DefaultFormatter
+        method.call
+      end
+    end
     describe ".story" do
       let(:story) {double :story}
+      let(:method) { lambda{ Main.story 32 } }
       before(:each) do
         Story.stub(:find).and_return story
         DefaultFormatter.stub(:new).and_return writer
       end
+
+      include_examples "formatter fetching"
       
       it "initiates a story" do
         Story.should_receive(:find).with(23).and_return story
@@ -28,10 +41,12 @@ module PivotalToPdf
     describe ".current_iteration" do
       let(:stories) {double :stories_from_iteration}
       let(:iteration) {double :iteration, :stories => stories}
+      let(:method) { lambda{ Main.current_iteration } }
       before(:each) do
         Iteration.stub(:find).and_return [iteration, double]
         DefaultFormatter.stub(:new).and_return writer
       end
+      include_examples "formatter fetching"
       
       it "initiates an Iteration" do
         Iteration.should_receive(:find).with(:all, :params => {:group => "current"}).and_return [iteration]
@@ -52,10 +67,13 @@ module PivotalToPdf
     describe ".iteration" do
       let(:stories) {double :stories_from_iteration}
       let(:iteration) {double :iteration, :stories => stories}
+      let(:method) { lambda{ Main.iteration 13 } }
       before(:each) do
         Iteration.stub(:find).and_return [iteration, double]
         DefaultFormatter.stub(:new).and_return writer
       end
+
+      include_examples "formatter fetching"
 
       it "fetches an Iteration, given the Iteration number (you have to minus to get the accurate iteration)" do
         Iteration.should_receive(:find).with(:all, :params => {:offset => 12, :limit => 1}).and_return [iteration]
@@ -75,10 +93,12 @@ module PivotalToPdf
 
     describe ".label" do
       let(:story) {double :story}
+      let(:method) { lambda{ Main.label "testing" } }
       before(:each) do
         Story.stub(:find).and_return story
         DefaultFormatter.stub(:new).and_return writer
       end
+      include_examples "formatter fetching"
       
       it "initiates a label search" do
         # Kind of a null test here, not sure how else to spec it
