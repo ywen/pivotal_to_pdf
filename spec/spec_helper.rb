02b3db3397961f84d62cbea6e1d5511aeacd9c82
Yi Wen <ywen.github@gmail.com>
2015-05-25 17:05:51 -0500

    Fixing spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,13 @@
 require 'rubygems'
-require 'spork'
 require 'rspec/core'
 
-Spork.prefork do
-  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-  RSpec.configure do |config|
-    config.expect_with :rspec do |c|
-      c.syntax = :expect
-    end
-    config.mock_with :rspec
-    config.extend Macros
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
   end
-
+  config.mock_with :rspec
+  config.extend Macros
 end
-
-Spork.each_run do
-  # Requires supporting files with custom matchers and macros, etc,
-  # in ./support/ and its subdirectories.
-  require "#{File.dirname(__FILE__)}/../lib/pivotal_to_pdf"
-  Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each {|f| require f}
-end
+require "#{File.dirname(__FILE__)}/../lib/pivotal_to_pdf"
+Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each {|f| require f}
